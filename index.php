a8a690f7260729ea3b82c295bef293b4f13e8cc0
DevSteven1 <stevenpajarodev@gmail.com>
2024-05-26 13:13:42 -0500

    first commit

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Restaurante</title>
+    <script src="./tailwindcss.js"></script>
+</head>
+<body class="h-screen flex justify-center items-center">
+    <div class="h-[90%] w-[90%] bg-black"></div>
+</body>
+</html>
---
+++ pages/Menu.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Menu</title>
+    <script src="../tailwindcss.js"></script>
+</head>
+<body class="grid grid-cols-12 w-screen h-screen p-10">
+    <aside class="col-span-2 bg-blue-300 p-4 grid grid-rows-12 rounded-s-lg">
+        <div class="row-span-11 bg-green-500 rounded-lg"></div>
+        <div class="row-span-1 flex justify-center items-center">
+            <input type="submit" class="text-xl border rounded-lg h-[70%] w-[100%]" value="ORDENAR"></input>
+        </div>
+    </aside>
+    <section class="col-span-10 bg-green-200 p-4 rounded-e-lg">g</section>
+</body>
+</html>
